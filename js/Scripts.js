6d14661bcf560d345364b48a6736f843c3706c30
Tom <ThomasBoyd2013@hotmail.co.uk>
2018-10-29 13:25:13 -0400

    some fixes

--- js/Scripts.js
+++ js/Scripts.js
@@ -94,7 +94,7 @@ function random() {
 //Global vars to do with map zooming
 var to, from = [];
 var per, sto, sfrom, hto, hfrom = 0;
-var svg, svgStyle, lines;
+var svg, svgStyle, lines, zooming;
 var view = "map";
 //mapZoom gets a new value using a percentage of
 //what the value started as, to what the value will be.
@@ -111,6 +111,7 @@ function mapZoom() {
 	if (val[3] < 0) val[3] = 0;
 	if (per >= 1) {
 		svg.setAttribute("viewBox", to[0]+" "+to[1]+" "+to[2]+" "+to[3]);
+		zooming = false;
 		if (view == "square") hideGrid();
 		return;
 	} else svg.setAttribute("viewBox", val[0]+" "+val[1]+" "+val[2]+" "+val[3]);
@@ -131,56 +132,62 @@ function showGrid() {
 
 //These 3 functions can show the 3 different views
 function viewSquare(info){
-	view = "square";
-	var map = document.getElementById("map");
-	var square = document.getElementById("square");
-	var feature = document.getElementById("feature");
-	var artifacts = document.getElementById("artifacts");
-	artifacts.style.display = "inline";
-	map.style.display = "none";
-	feature.style.display = "none";
-	square.style.display = "inline";
+	if (!zooming) {
+		view = "square";
+		zooming = true;
+		var map = document.getElementById("map");
+		var square = document.getElementById("square");
+		var feature = document.getElementById("feature");
+		var artifacts = document.getElementById("artifacts");
+		artifacts.style.display = "inline";
+		map.style.display = "none";
+		feature.style.display = "none";
+		square.style.display = "inline";
 
-	//Set value for zooming in
-	per = 0;
-	var string = info.split("S")[1];
-	var first = parseInt(string.split("R")[0]);
-	var second = parseInt(string.split("R")[1]);
-	if (second == 103) second = 110;
-	first = (first+10)*-1;
-	second = second-10;
-	to = [second,first,10,150]; //viewbox
-	from = [-20, -330, 130, 150]; //viewbox
-	sto = 0.05; //stroke
-	sfrom = 0.3; //stroke
-	hto = 494; //height
-	hfrom = parseInt(svgStyle.style.height); //height
-	mapZoom();
+		//Set value for zooming in
+		per = 0;
+		var string = info.split("S")[1];
+		var first = parseInt(string.split("R")[0]);
+		var second = parseInt(string.split("R")[1]);
+		if (second == 103) second = 110;
+		first = (first+10)*-1;
+		second = second-10;
+		to = [second,first,10,150]; //viewbox
+		from = [-20, -330, 130, 150]; //viewbox
+		sto = 0.05; //stroke
+		sfrom = 0.3; //stroke
+		hto = 494; //height
+		hfrom = 570; //height
+		mapZoom();
+	}
 }
 function viewMap() {
-	view = "map";
-	showGrid();
-	var map = document.getElementById("map");
-	var square = document.getElementById("square");
-	var feature = document.getElementById("feature");
-	var artifacts = document.getElementById("artifacts");
-	artifacts.style.display = "none";
-	map.style.display = "inline";
-	feature.style.display = "none";
-	square.style.display = "none";
+	if (!zooming) {
+		view = "map";
+		zooming = true;
+		showGrid();
+		var map = document.getElementById("map");
+		var square = document.getElementById("square");
+		var feature = document.getElementById("feature");
+		var artifacts = document.getElementById("artifacts");
+		artifacts.style.display = "none";
+		map.style.display = "inline";
+		feature.style.display = "none";
+		square.style.display = "none";
 
-	//Reverses all values for map zooming
-	per = 0;
-	var temp = to;
-	to = from;
-	from = temp;
-	temp = sto;
-	sto = sfrom;
-	sfrom = temp;
-	temp = hto;
-	hto = hfrom;
-	hfrom = temp;
-	mapZoom();
+		//Reverses all values for map zooming
+		per = 0;
+		var temp = to;
+		to = from;
+		from = temp;
+		temp = sto;
+		sto = sfrom;
+		sfrom = temp;
+		temp = hto;
+		hto = hfrom;
+		hfrom = temp;
+		mapZoom();
+	}
 }
 function viewFeature() {
 	var map = document.getElementById("map");
